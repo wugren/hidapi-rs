f19a1e887f613dd29868bea3f709a5399e97049c
Roland Ruckerbauer <roland.rucky@gmail.com>
2015-07-28 01:03:29 +0200

    Fixed wrong function prototypes.

--- src/ffi.rs
+++ src/ffi.rs
@@ -15,7 +15,7 @@ pub struct HidDeviceInfo {
     usage_page: c_ushort,
     usage: c_ushort,
     interface_number: c_int,
-    next: *const HidDeviceInfo,
+    next: *mut HidDeviceInfo,
 }
 
 #[cfg(target_os = "windows")]
@@ -23,30 +23,30 @@ pub struct HidDeviceInfo {
 extern "C" {
     pub fn hid_init() -> c_int;
     pub fn hid_exit() -> c_int;
-    pub fn hid_enumerate(vendor_id: c_ushort, product_id: c_ushort) -> *const HidDeviceInfo;
-    pub fn hid_free_enumeration(hid_device_info: *const HidDeviceInfo);
+    pub fn hid_enumerate(vendor_id: c_ushort, product_id: c_ushort) -> *mut HidDeviceInfo;
+    pub fn hid_free_enumeration(hid_device_info: *mut HidDeviceInfo);
     pub fn hid_open(vendor_id: c_ushort, product_id: c_ushort, serial_number: *const wchar_t)
             -> *const HidDevice;
-    pub fn hid_open_path(path: *const c_char) -> *const HidDevice;
-    pub fn hid_write(device: *const HidDevice, data: *const c_uchar, length: size_t) -> c_int;
-    pub fn hid_read_timeout(device: *const HidDevice, data: *const c_uchar, length: size_t,
+    pub fn hid_open_path(path: *const c_char) -> *mut HidDevice;
+    pub fn hid_write(device: *mut HidDevice, data: *const c_uchar, length: size_t) -> c_int;
+    pub fn hid_read_timeout(device: *mut HidDevice, data: *mut c_uchar, length: size_t,
             milleseconds: c_int) -> c_int;
-    pub fn hid_read(device: *const HidDevice, data: *const c_uchar, length: size_t) -> c_int;
-    pub fn hid_set_nonblocking(device: *const HidDevice, nonblock: c_int) -> c_int;
-    pub fn hid_send_feature_report(device: *const HidDevice, data: *const c_uchar, length: size_t)
+    pub fn hid_read(device: *mut HidDevice, data: *mut c_uchar, length: size_t) -> c_int;
+    pub fn hid_set_nonblocking(device: *mut HidDevice, nonblock: c_int) -> c_int;
+    pub fn hid_send_feature_report(device: *mut HidDevice, data: *const c_uchar, length: size_t)
             -> c_int;
-    pub fn hid_get_feature_report(device: *const HidDevice, data: *const c_uchar, length: size_t)
+    pub fn hid_get_feature_report(device: *mut HidDevice, data: *mut c_uchar, length: size_t)
             -> c_int;
-    pub fn hid_close(device: *const HidDevice);
-    pub fn hid_get_manufacturer_string(device: *const HidDevice, string: *const wchar_t,
+    pub fn hid_close(device: *mut HidDevice);
+    pub fn hid_get_manufacturer_string(device: *mut HidDevice, string: *mut wchar_t,
             maxlen: size_t) -> c_int;
-    pub fn hid_get_product_string(device: *const HidDevice, string: *const wchar_t, maxlen: size_t)
+    pub fn hid_get_product_string(device: *mut HidDevice, string: *mut wchar_t, maxlen: size_t)
             -> c_int;
-    pub fn hid_get_serial_number_string(device: *const HidDevice, string: *const wchar_t,
+    pub fn hid_get_serial_number_string(device: *mut HidDevice, string: *mut wchar_t,
             maxlen: size_t) -> c_int;
-    pub fn hid_get_indexed_string(device: *const HidDevice, string_index: c_int,
-            string: *const wchar_t, maxlen: size_t) -> c_int;
-    pub fn hid_error(device: *const HidDevice) -> *const wchar_t;
+    pub fn hid_get_indexed_string(device: *mut HidDevice, string_index: c_int,
+            string: *mut wchar_t, maxlen: size_t) -> c_int;
+    pub fn hid_error(device: *mut HidDevice) -> *const wchar_t;
 }
 
 #[cfg(target_os = "linux")]
