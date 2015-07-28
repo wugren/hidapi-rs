be2ca04c2132c92e986afa7246b0f2e5f2859452
Roland Ruckerbauer <roland.rucky@gmail.com>
2015-07-28 21:05:43 +0200

    Implemented unsafe global initialization.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 extern crate libc;
 
 mod ffi;
+
+use std::sync::{ONCE_INIT, Once};
+
+static mut INIT: Once = ONCE_INIT;
+
+#[inline(always)]
+unsafe fn init() {
+    INIT.call_once(||{
+        ffi::hid_init();
+    });
+}
+
+pub struct HidDevice {
+    _c_struct: *mut ffi::HidDevice,
+}
+
+impl HidDevice {
+
+}
