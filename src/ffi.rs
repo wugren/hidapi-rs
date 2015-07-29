33eca33238ab702bcb948d7a28cb658b477539ee
Roland Ruckerbauer <roland.rucky@gmail.com>
2015-07-29 17:10:32 +0200

    Added legal notice and license.

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,3 +1,22 @@
+/****************************************************************************
+    Copyright (c) 2015 Roland Ruckerbauer All Rights Reserved.
+
+    This file is part of hidapi_rust.
+
+    hidapi_rust is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    hidapi_rust is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with hidapi_rust.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
 // For documentation look at the corresponding C header file hidapi.h
 
 use libc::{c_void, c_char, c_ushort, wchar_t, c_int, c_uchar, size_t};
