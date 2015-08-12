5756eb1794d57b48cd643f6efda4d15374b1cac5
Roland Ruckerbauer <roland.rucky@gmail.com>
2015-08-12 14:47:18 +0200

    Added copyright where missing.

--- build.rs
+++ build.rs
@@ -1,3 +1,22 @@
+/****************************************************************************
+    Copyright (c) 2015 Roland Ruckerbauer All Rights Reserved.
+
+    This file is part of hidapi_rust.
+
+    hidapi_rust is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    hidapi_rust is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with hidapi_rust.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
 extern crate gcc;
 extern crate pkg_config;
 
