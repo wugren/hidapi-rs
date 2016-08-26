191f5c4f5d1c00c6da058023c80f7967003c97d9
Artyom Pavlov <newpavlov@gmail.com>
2016-08-26 20:42:17 +0300

    fmt on build.rs

--- build.rs
+++ build.rs
@@ -1,21 +1,21 @@
-/****************************************************************************
-    Copyright (c) 2015 Roland Ruckerbauer All Rights Reserved.
-
-    This file is part of hidapi_rust.
-
-    hidapi_rust is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    hidapi_rust is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with hidapi_rust.  If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
+// **************************************************************************
+// Copyright (c) 2015 Roland Ruckerbauer All Rights Reserved.
+//
+// This file is part of hidapi_rust.
+//
+// hidapi_rust is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// hidapi_rust is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with hidapi_rust.  If not, see <http://www.gnu.org/licenses/>.
+// *************************************************************************
 
 extern crate gcc;
 extern crate pkg_config;
@@ -38,18 +38,18 @@ fn compile() {
 #[cfg(target_os = "windows")]
 fn compile() {
     gcc::Config::new()
-            .file("etc/hidapi/windows/hid.c")
-            .include("etc/hidapi/hidapi")
-            .compile("libhidapi.a");
+        .file("etc/hidapi/windows/hid.c")
+        .include("etc/hidapi/hidapi")
+        .compile("libhidapi.a");
     println!("cargo:rustc-link-lib=setupapi");
 }
 
 #[cfg(target_os = "macos")]
 fn compile() {
     gcc::Config::new()
-            .file("etc/hidapi/mac/hid.c")
-            .include("etc/hidapi/hidapi")
-            .compile("libhidapi.a");
+        .file("etc/hidapi/mac/hid.c")
+        .include("etc/hidapi/hidapi")
+        .compile("libhidapi.a");
     println!("cargo:rustc-link-lib=framework=IOKit");
     println!("cargo:rustc-link-lib=framework=CoreFoundation");
 }
