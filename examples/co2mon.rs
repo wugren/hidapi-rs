7ce4b25811fdd5a908e18b41ffa98b47485bde35
Osspial <osspial@gmail.com>
2017-07-17 15:37:39 -0400

    Relicense under MIT license

--- examples/co2mon.rs
+++ examples/co2mon.rs
@@ -3,24 +3,11 @@
 
     This file is part of hidapi-rs, based on hidapi_rust by Roland Ruckerbauer.
     It's also based on the Oleg Bulatov's work (https://github.com/dmage/co2mon)
-
-    hidapi-rs is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    hidapi-rs is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with hidapi-rs.  If not, see <http://www.gnu.org/licenses/>.
 ****************************************************************************/
 
 
-//! Opens a KIT MT 8057 CO2 detector and reads data from it. This 
-//! example will not work unless such an HID is plugged in to your system. 
+//! Opens a KIT MT 8057 CO2 detector and reads data from it. This
+//! example will not work unless such an HID is plugged in to your system.
 
 extern crate hidapi;
 
