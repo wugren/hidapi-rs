6382019aa6290f1540750c13508c7a1782bf9ec0
Roland Ruckerbauer <roland.rucky@gmail.com>
2017-11-20 00:55:26 +0100

    Updated dependencies, replaced gcc crate with cc

--- build.rs
+++ build.rs
@@ -17,7 +17,7 @@
 // along with hidapi_rust.  If not, see <http://www.gnu.org/licenses/>.
 // *************************************************************************
 
-extern crate gcc;
+extern crate cc;
 extern crate pkg_config;
 
 fn main() {
@@ -26,7 +26,7 @@ fn main() {
 
 #[cfg(target_os = "linux")]
 fn compile() {
-    let mut config = gcc::Config::new();
+    let mut config = cc::Build::new();
     config.file("etc/hidapi/libusb/hid.c").include("etc/hidapi/hidapi");
     let lib = pkg_config::find_library("libusb-1.0").expect("Unable to find libusb-1.0");
     for path in lib.include_paths {
@@ -37,7 +37,7 @@ fn compile() {
 
 #[cfg(target_os = "windows")]
 fn compile() {
-    gcc::Config::new()
+    cc::Build::new()
         .file("etc/hidapi/windows/hid.c")
         .include("etc/hidapi/hidapi")
         .compile("libhidapi.a");
@@ -46,7 +46,7 @@ fn compile() {
 
 #[cfg(target_os = "macos")]
 fn compile() {
-    gcc::Config::new()
+    cc::Build::new()
         .file("etc/hidapi/mac/hid.c")
         .include("etc/hidapi/hidapi")
         .compile("libhidapi.a");
