b1851167755993a5a2cbd7d5f25ccabbfa75e9d7
Roland Ruckerbauer <mail@ruabmbua.dev>
2023-05-06 16:21:00 +0200

    Fix freebsd build
    libusb version of freebsd is too old and does not support
    libusb_set_option()

--- src/ffi.rs
+++ src/ffi.rs
@@ -42,7 +42,7 @@ extern "C" {
     pub fn hid_open_path(path: *const c_char) -> *mut HidDevice;
     #[cfg(libusb)]
     pub fn hid_libusb_wrap_sys_device(sys_dev: intptr_t, interface_num: c_int) -> *mut HidDevice;
-    #[cfg(libusb)]
+    #[cfg(all(libusb, not(target_os = "freebsd")))]
     pub fn libusb_set_option(ctx: *mut LibusbContext, option: c_int);
     pub fn hid_write(device: *mut HidDevice, data: *const c_uchar, length: size_t) -> c_int;
     pub fn hid_read_timeout(
