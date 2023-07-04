8eddba972dfaed3f8e6c06de41946e0b36b16218
sidit77 <sidit77@gmail.com>
2023-07-04 12:52:31 +0200

    removed c build step from native backend

--- build.rs
+++ build.rs
@@ -186,21 +186,23 @@ fn compile_illumos() {
 }
 
 fn compile_windows() {
-    let linkage = env::var("CARGO_CFG_TARGET_FEATURE").unwrap_or_default();
-
-    let mut cc = cc::Build::new();
-    cc.file("etc/hidapi/windows/hid.c")
-        .include("etc/hidapi/hidapi");
-
-    if linkage.contains("crt-static") {
-        // https://doc.rust-lang.org/reference/linkage.html#static-and-dynamic-c-runtimes
-        cc.static_crt(true);
-    }
-    cc.compile("libhidapi.a");
-    println!("cargo:rustc-link-lib=setupapi");
-
     #[cfg(not(feature = "windows-native"))]
-    println!("cargo:rustc-cfg=hidapi");
+    {
+        let linkage = env::var("CARGO_CFG_TARGET_FEATURE").unwrap_or_default();
+
+        let mut cc = cc::Build::new();
+        cc.file("etc/hidapi/windows/hid.c")
+            .include("etc/hidapi/hidapi");
+
+        if linkage.contains("crt-static") {
+            // https://doc.rust-lang.org/reference/linkage.html#static-and-dynamic-c-runtimes
+            cc.static_crt(true);
+        }
+        cc.compile("libhidapi.a");
+        println!("cargo:rustc-link-lib=setupapi");
+
+        println!("cargo:rustc-cfg=hidapi");
+    }
 }
 
 fn compile_macos() {
