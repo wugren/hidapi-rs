0cec7e1b01602dfbabce15fcd6f96f262638ed59
Marek G <marek-dev@yandex.com>
2024-12-16 17:17:45 +0100

    Implement send_output_report() for linux-native

--- src/linux_native/ioctl.rs
+++ src/linux_native/ioctl.rs
@@ -7,6 +7,7 @@ const HIDRAW_IOC_MAGIC: u8 = b'H';
 const HIDRAW_IOC_GRDESCSIZE: u8 = 0x01;
 const HIDRAW_SET_FEATURE: u8 = 0x06;
 const HIDRAW_GET_FEATURE: u8 = 0x07;
+const HIDRAW_SET_OUTPUT: u8 = 0x0b;
 
 ioctl_read!(
     hidraw_ioc_grdescsize,
@@ -25,5 +26,11 @@ ioctl_read_buf!(
     hidraw_ioc_get_feature,
     HIDRAW_IOC_MAGIC,
     HIDRAW_GET_FEATURE,
+    u8
+);
+ioctl_write_buf!(
+    hidraw_ioc_set_output,
+    HIDRAW_IOC_MAGIC,
+    HIDRAW_SET_OUTPUT,
     u8
 );
